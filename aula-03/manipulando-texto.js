e4963008d3b5312367cbb19f0688d3bb62a6c82f
Beatriz Silva <126727432+BeatrizSRocha@users.noreply.github.com>
2023-09-22 11:50:12 -0300

    Add files via upload

---
+++ aula-03/manipulando-texto.js
@@ -0,0 +1,14 @@
+let numFloat = 123.543;
+let linguagens = ["Java", "C#", "Python"];
+                //  0      1        2
+let txt = "Estão chegando as provas"
+
+// Funções nativas: funções criadas no javascript
+
+//console.log(typeof numFloat.toString());
+
+// console.log(typeof String(numFloat));
+
+// console.log(linguagens[1]);
+
+console.log(txt.replace("provas", "avaliações"));
