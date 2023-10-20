95cc01d82f7188e0990b85cf225be557a0f3b47c
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 11:45:13 -0300

    Atualização

--- Aula-07/exercicio-casa.js
+++ Aula-07/exercicio-casa.js
@@ -6,4 +6,4 @@
     - Para ver se dey certo, exiba o upperCaseCities no console.
     */
    const cities = ["São Paulo","Rio de Janeiro","Minas Gerais"]
-   let upperCaseCities = []
+   let upperCaseCities = [];
