deeae02092be653450d5b2352bc2a03e2869bcd5
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-17 11:32:57 -0300

    Nova aula

---
+++ modal.js
@@ -0,0 +1,15 @@
+const button = document.querySelector("button");
+const poupup = document.querySelector(".poupup-wrapper");
+
+button.addEventListener("click",function(){
+    poupup.classList.add("d-block");
+});
+
+poupup.addEventListener("click",function(event){
+    const clickOfElement=event.target.classList[0];
+    const classNameArray=["poupup-cloase","pouup-link","pouup-wrap"]
+    const isClassList=classNameArray.includes(clickOfElement);
+    if (isClassList){
+        poupup.classList.remove("d-block");
+    }
+});
