a5f17f7c681b23198826f1a6fbc6a9a99fc1cac9
BeatrizSRocha <biasrocha@uol.com.br>
2024-01-03 12:28:56 -0300

    Atualizacao

--- animals.js
+++
@@ -1,7 +0,0 @@
-const animals=["Macaco","Tucano","Elefante","Pavão","Hipopótamo","Leão"];
-if(!(animals.includes("Leão"))){
-    console.log("Leão não existe em animals")
-}
-else{
-    console.log("Leão existe em animals")
-}
--- series.js
+++
@@ -1,7 +0,0 @@
-const minhasSeriesFavoritas=["Shardowhunters","Peaky Blinders","Friends"];
-if(minhasSeriesFavoritas.length === 3){
-    console.log("True")
-}
-else{
-    console.log("False")
-}
