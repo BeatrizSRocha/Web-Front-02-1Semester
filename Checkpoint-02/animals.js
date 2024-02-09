7d13ccfaa07d9a863bf6a7e67eee4989ff4b95de
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-09 11:02:18 -0300

    Nova aula

---
+++ Checkpoint-02/animals.js
@@ -0,0 +1,7 @@
+const animals=["Macaco","Tucano","Elefante","Pavão","Hipopótamo","Leão"];
+if(!(animals.includes("Leão"))){
+    console.log("Leão não existe em animals")
+}
+else{
+    console.log("Leão existe em animals")
+}
---
+++ Checkpoint-02/series.js
@@ -0,0 +1,7 @@
+const minhasSeriesFavoritas=["Shardowhunters","Peaky Blinders","Friends"];
+if(minhasSeriesFavoritas.length === 3){
+    console.log("True")
+}
+else{
+    console.log("False")
+}
