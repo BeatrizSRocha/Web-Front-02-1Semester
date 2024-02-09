fbf46b32865a1c175dd7dbe0039d59958cb9e7a6
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-09 10:57:27 -0300

    Nova aula

---
+++ Checkpoint-01/script.js
@@ -0,0 +1,29 @@
+// Variáveis
+let nome = "Beatriz";
+let idade = 20;
+let fome = true;
+let valorproduto = 7.85;
+let times = ["Corinthians", "São Paulo", "Palmeiras"];
+
+// Consoles
+console.log(nome);
+console.log(idade);
+console.log(fome);
+console.log(valorproduto);
+console.log(times);
+
+// 7. Operações Matemáticas
+let numero1 = 4;
+let numero2 = 2;
+let numero3 = 20;
+let numero4 = 8;
+
+let soma = numero1 + numero2 + numero3 + numero4;
+let subtracao = numero1 - numero2 - numero3 - numero4;
+let multiplicacao = numero1 * numero2 * numero3 * numero4;
+let divisao = numero1 / numero2 / numero3 / numero4;
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
