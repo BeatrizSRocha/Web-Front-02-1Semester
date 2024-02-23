bfe2e1922d481f6ab620fca1fee530c2cc730bfb
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-23 11:48:48 -0300

    Nova aula

--- Aula-03/Teste/teste.js
+++ Aula-03/Teste/teste.js
@@ -1,13 +1,23 @@
 const btn1=document.querySelector('button')
+
 // Pega a lista de filmes
 const listaFilmes=document.querySelector('#listaFilmes')
+
 // Cria o elemento li
 const itemLista=document.createElement('li') // Create Element => cria um novo elemento
 
+// Pega o input
+const textoUsuario=document.querySelector('#filmeInput')
+
 btn1.addEventListener('click',()=>{
     listaFilmes.append(itemLista) // Adicionando li na lista
-    itemLista.innerHTML='Harry Potter'
+    itemLista.innerHTML=textoUsuario.value
 })
+
+// btn1.addEventListener('click',()=>{
+//     listaFilmes.append(itemLista) // Adicionando li na lista
+//     itemLista.innerHTML='Harry Potter'
+// })
 
 // btn1.addEventListener('click',()=>{
 //     window.alert("Clicouu!")
