53e1883b1ddf38f38c9f47a7413b2b478564fc78
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-08 11:39:17 -0300

    Nova aula

---
+++ Aula-05/script.js
@@ -0,0 +1,21 @@
+// document.cookie="userName=Bia"
+
+// document.cookie = "username=Bia;expires=Thu, 14 Dec 2034 12:00:00 UTC; path=/";
+
+// ---- //
+
+// let nome='Bia'
+// localStorage.setItem('nome', nome) // LocalStorage só armazena string
+
+// console.log(localStorage.getItem('nome'))
+// localStorage.removeItem
+
+// ---- //
+
+// let nome=['Caio','Carol','Pedro']
+// let nome Json=JSON.stringify(nome)
+// localStorage.setItem('nome',nomeJson)
+
+// const nomes=JSON.parse(localStorage.getItem(nomeJson))
+
+// https://github.com/CavMCarolina/Atividades-JS/commits?author=caiooliveira-tech
